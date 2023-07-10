2c3417f3ddca99b869b1aa3320d51907ae55b884
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-07-11 00:53:24 +0800

    added to server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+// Import required packages and modules
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+
+// Set up the Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up the session
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Set up the Handlebars engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Come check the funk at http://localhost:${PORT}`);
+});
