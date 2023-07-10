2bdb2f341be4d08d00fb99641bd9f7fa0fb56f18
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-07-11 01:05:14 +0800

    added sequelize config

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const routes = require('./controllers');
+const sequelize = require('./config/connection')
+//const routes = require('./controllers');
 
 // Set up the Express app
 const app = express();
@@ -17,8 +18,8 @@ app.use(session({
 }));
 
 // Set up the Handlebars engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+//app.set('view engine', 'handlebars');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -26,9 +27,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use(routes);
+//app.use(routes);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Come check the funk at http://localhost:${PORT}`);
-});
+
+// Sync db
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Come check the funk at http://localhost:${PORT}`));
+  });
